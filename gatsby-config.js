4ee0443aa7c2a89a9012d624e829c0876f6a3fb5
Matt Uttridge <matt@certua.io>
2017-11-10 11:39:02 +0000

    Connect to CMS and get shapes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+      `gatsby-plugin-react-helmet`,
+      {
+        resolve: `gatsby-source-contentful`,
+        options: {
+          spaceId: `p9wvos6zyimj`,
+          accessToken: `7f1a358e5546a3760c910aeb5acb8dbf29f359561d6f4d06a6c02374e700a292`
+        }
+      }
+  ],
 }
