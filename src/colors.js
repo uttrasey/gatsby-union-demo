9334781adc6cfd9b160e242a3f7c686631f855e0
Matt Uttridge <matt@certua.io>
2017-11-10 13:41:47 +0000

    Squares

---
+++ src/colors.js
@@ -0,0 +1,24 @@
+const getRandomColor = () => {
+  var letters = '0123456789ABCDEF'
+  var color = '#'
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+const invertColor = (hexTripletColor) => {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+}
+
+export {
+  getRandomColor,
+  invertColor
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,66 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+import {  getRandomColor, invertColor } from '../colors'
 
-const IndexPage = () => (
+const Shapes = styled.article`
+  width: 100%;
+  text-align: center;
+`
+
+const Wrap = styled.div`
+    & div {
+      background-color: ${props => props.bgcolor};
+    }
+`
+
+
+const Square = styled.div`
+  width: ${props => `${props.sideLength}px`};
+  height: ${props => `${props.sideLength}px`};
+  margin: 20px auto;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+  box-shadow: 5px 5px 2px 1px rgba(0, 0, 0, .2);
+`
+
+const Area = styled.span`
+  font-size : 12px;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+`
+
+const IndexPage = ({ data }) => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Shapes ordered by area</h1>
+    <Shapes>
+    {
+      data.squares.edges.map(
+        ({ node }) =>
+          <Wrap key={node.id} bgcolor={getRandomColor()}>
+            <Square {...node} />
+            <Area>◻️ {node.sideLength * node.sideLength} ({node.sideLength} x {node.sideLength})</Area>
+          </Wrap>
+      )
+    }
+    </Shapes>
   </div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexQuery {
+    squares: allContentfulSquare {
+      edges {
+        node {
+          sideLength
+          id
+        }
+      }
+    }
+  }
+`
