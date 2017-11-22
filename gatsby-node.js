855e0e035be914257d0ee2aa20a13b398e94e81e
Matt Uttridge <uttrasey@gmail.com>
2017-11-22 12:08:07 +0000

    Area union added in gatsby site

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const _ = require(`lodash`)
+const { GraphQLUnionType, GraphQLList } = require(`graphql`)
+
 const cmsPrefix = `Contentful`
 
 const getCircleArea = radius => {
@@ -23,4 +26,56 @@ exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
       value: getCircleArea(node.radius)
     })
   }
+}
+
+/**
+ * The GraphQL definition of our shape union type
+ */
+const defineShapesType = (CircleGql, SquareGql) => (
+  new GraphQLUnionType({
+    name: `AllShapesType`,
+    description: `All shapes under one roof`,
+    types: [ CircleGql, SquareGql ],
+    resolveType: (data) => {
+      const { type } = data.internal
+      switch (type) {
+        case `ContentfulCircle`:
+          return CircleGql
+        case `ContentfulSquare`:
+          return SquareGql
+        default:
+          return null
+      }
+    },
+  })
+)
+
+const genAllShapesField = (CircleGql, SquareGql, allNodes) => {
+  return {
+    allShapes: {
+      type: new GraphQLList(defineShapesType(CircleGql, SquareGql)),
+      args: {},
+      resolve(root, args) {
+        const latestNodes = _.filter(
+          allNodes,
+          n => (n.internal.type === `ContentfulCircle`) ||
+                (n.internal.type === `ContentfulSquare`)
+        )
+        return latestNodes
+      },
+    },
+  }
+}
+
+/**
+ * Hook up the Union Query
+ */
+exports.enhanceSchema = ({ types, allNodes}) => {
+  const CircleType = types[`contentfulCircle`].nodeObjectType
+  const SqaureType = types[`contentfulSquare`].nodeObjectType
+  console.log('here');
+  return new Promise((resolve, reject) => {
+    const allShapes = genAllShapesField(CircleType, SqaureType, allNodes);
+    resolve(allShapes)
+  });
 }
